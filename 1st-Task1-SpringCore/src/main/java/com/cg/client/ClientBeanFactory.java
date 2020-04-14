cff298bf66c385313e858c2bc15407c2ce286ce5
andy077 <37210279+andy077@users.noreply.github.com>
2020-04-14 22:15:59 +0530

    Add files via upload

---
+++ 1st-Task1-SpringCore/src/main/java/com/cg/client/ClientBeanFactory.java
@@ -0,0 +1,24 @@
+package com.cg.client;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import com.cg.service.IService;
+
+public class ClientBeanFactory {
+
+	static BeanFactory fac;
+	
+	static {
+		Resource res= new ClassPathResource("beans.xml");
+		fac= new XmlBeanFactory(res);
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Main starts");
+		IService object = fac.getBean("serviceBean", IService.class);
+		System.out.println(object.viewName());
+	}
+}
